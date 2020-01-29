c3733518e81a345ca484b9fd9af8e6eb100c409b
Ritvick-me <pandey.ritvick.v@gmail.com>
2020-01-29 18:44:32 +0530

    minor changes.

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -51,3 +51,16 @@ $('.kz-mid-sec').hover(
 );
 
 //Middle Section background animation ends here----------
+
+//Form starts here---------------------------------------
+
+function restrictAlphabets(e){
+  var x=e.which||e.keycode;
+  if((x>=48 && x<=57) || x==8 ||
+    (x>=35 && x<=40)|| x==46)
+    return true;
+  else
+    return false;
+}
+
+//FOrm ends here-----------------------------------------
